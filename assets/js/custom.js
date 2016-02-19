3aef6e3cfcdfce96483208f58f86bb8fb75c7f58
eljun <el.junsan@yahoo.com>
2016-02-19 13:06:41 +0800

    Added: Cookies script, mailchimp account & some fixes in the footer

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,24 +1,34 @@
 $(document).ready(function () {
 
-$('.services p').expander({
-	slicePoint: 120,
-	expandText: 'more',
-	userCollapseText: 'less'
-})
-// Highlight the top nav as scrolling
-$('body').scrollspy({
-    target: '.navbar-fixed-top',
-    offset: 80
-})
+    $('.services p').expander({
+    	slicePoint: 120,
+    	expandText: 'more',
+    	userCollapseText: 'less'
+    });
+    // Highlight the top nav as scrolling
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 80
+    });
 
-// Page scrolling feature
-$('a.page-scroll').bind('click', function(event) {
-    var link = $(this);
-    $('html, body').stop().animate({
-        scrollTop: $(link.attr('href')).offset().top - 70
-    }, 500);
-    event.preventDefault();
-});
+    // Page scrolling feature
+    $('a.page-scroll').bind('click', function(event) {
+        var link = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(link.attr('href')).offset().top - 70
+        }, 500);
+        event.preventDefault();
+    });
+
+    // Herbycookies Here
+
+    $(document).herbyCookie({
+        policyText: "Lear more",
+        text: "Cookies help us deliver our services. By using our services, you agree to our use of cookies.",
+        link: "/cookies",
+        expiryDays: 180
+    });
+
 
 });
 
