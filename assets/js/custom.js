ff362da4ce7f9cd459386a983708f6d2d8f9eb7e
allen_support <allen.support@webfactories.biz>
2016-10-27 17:11:08 +0800

    update font weight, custom.js and layout

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -150,25 +150,25 @@ function masonry(element){
 	});
 }
 
-function onReady(callback) {
-	var intervalID = window.setInterval(checkReady, 1000);
+// function onReady(callback) {
+// 	var intervalID = window.setInterval(checkReady, 1000);
 
-	function checkReady() {
-		if (document.getElementsByTagName('body')[0] !== undefined) {
-			window.clearInterval(intervalID);
-			callback.call(this);
-		}
-	}
-	}
+// 	function checkReady() {
+// 		if (document.getElementsByTagName('body')[0] !== undefined) {
+// 			window.clearInterval(intervalID);
+// 			callback.call(this);
+// 		}
+// 	}
+// 	}
 
-	function show(id, value) {
-		document.getElementById(id).style.display = value ? 'block' : 'none';
-	}
+// 	function show(id, value) {
+// 		document.getElementById(id).style.display = value ? 'block' : 'none';
+// 	}
 
-	onReady(function () {
-		show('page', true);
-		show('loading', false);
-	});
+// 	onReady(function () {
+// 		show('page', true);
+// 		show('loading', false);
+// 	});
 
 
 
