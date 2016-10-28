d9a84d66d2be21b2a5edd5fd9f2977f061c5f165
allen_support <allen.support@webfactories.biz>
2016-10-28 10:03:31 +0800

    remove some images that not neccessary

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,52 +1,7 @@
 $(document).ready(function () {
 
-	// Highlight the top nav as scrolling
-	// $('body').scrollspy({
-	//     target: '.navbar-fixed-top',
-	//     offset: 80
-	// });
+	$('body').find('a[href="#"]').click(function(e){return false;});
 
-	// Remove menu on click
-	// $('#navbar').find('a').click(function(){
-	//     $('#navbar').collapse('hide');
-	// });
-
-	// Modified Version
-	$(document).on('click', '.navbar-collapse.in', function(e) {
-		if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
-			$(this).collapse('hide');
-		}
-	});
-
-	$(document).bind('click', function(e) {
-	if($(e.target).closest('#navbar').length == 0) {
-			// click happened outside of menu, hide any visible menu items
-			$('#navbar').collapse('hide');
-		}
-	});
-
-	$('body').find('a[href="#"]').click(function(e){
-
-		return false;
-
-	});
-
-	// Page scrolling feature
-	// $('a.page-scroll').bind('click', function(event) {
-	//     var link = $(this);
-	//     $('html, body').stop().animate({
-	//         scrollTop: $(link.attr('href')).offset().top - 70
-	//     }, 500);
-	//     event.preventDefault();
-	// });
-
-	// // Herbycookies Here
-	// $(document).herbyCookie({
-	//     policyText: "Lear more",
-	//     text: "Cookies help us deliver our services. By using our services, you agree to our use of cookies.",
-	//     link: "/cookies",
-	//     expiryDays: 180
-	// });
 	$("nav").navigation();
 
 	$("nav").on("open.navigation", function() {
@@ -61,20 +16,6 @@ $(document).ready(function () {
 	});
 
 
-
-	// $(".bg").interactive_bg();
-
-	// var movementStrength = 25;
-	// var height = movementStrength / $(window).height();
-	// var width = movementStrength / $(window).width();
-	// $(".slider").mousemove(function(e){
-	// 		  var pageX = e.pageX - ($(window).width() / 2);
-	// 		  var pageY = e.pageY - ($(window).height() / 2);
-	// 		  var newvalueX = width * pageX * -1 - 25;
-	// 		  var newvalueY = height * pageY * -1 - 50;
-	// 		  $('.slider').css("background-position", newvalueX+"px     "+newvalueY+"px");
-	// });
-
 	$('#toolsTab').easyResponsiveTabs({
 		type: 'default', //Types: default, vertical, accordion
 		width: 'auto', //auto or any width like 600px
@@ -86,11 +27,15 @@ $(document).ready(function () {
 
 });
 
-// $('.services p').expander({
-// 		slicePoint: 110,
-// 		expandText: 'more',
-// 		userCollapseText: 'less'
-// });
+
+function setRandomInterval(){
+    var counter;
+    var total;
+    counter = Math.floor((Math.random()*(20-15+1)+ 15) );
+    total = counter + "000";
+    return total;
+}
+
 
 // Popup
 $('[data-toggle="popover"]').popover();
@@ -126,79 +71,6 @@ document.addEventListener('DOMContentLoaded', function(event) {
 }, 120000);
 
 
-// Activate WOW.js plugin for animation on scroll
- wow = new WOW(
-      {
-        animateClass: 'animated',
-        offset:       100,
-        callback:     function(box) {
-          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
-        }
-      }
-    );
-    wow.init();
-
-// _slaask.init('95426c2790f8f6fed51b9caab536ed57');
-
-function masonry(element){
-	var container = $(element).isotope({
-	  itemSelector: ".entries",
-	  layoutMode: 'masonry'
-	});
-	container.imagesLoaded().progress(function(){
-		container.isotope('layout');
-	});
-	$(window).scroll(function () {
-		if ($(window).scrollTop() >= $(document).height() - $(window).height() - 300) {
-		  container.imagesLoaded().progress(function(){
-		  container.isotope('layout');
-		  });
-		}
-	});
-}
-
-// function onReady(callback) {
-// 	var intervalID = window.setInterval(checkReady, 1000);
-
-// 	function checkReady() {
-// 		if (document.getElementsByTagName('body')[0] !== undefined) {
-// 			window.clearInterval(intervalID);
-// 			callback.call(this);
-// 		}
-// 	}
-// 	}
-
-// 	function show(id, value) {
-// 		document.getElementById(id).style.display = value ? 'block' : 'none';
-// 	}
-
-// 	onReady(function () {
-// 		show('page', true);
-// 		show('loading', false);
-// 	});
-
-
-
-// function isScrolledIntoView(elem) {
-//     var docViewTop = $(window).scrollTop();
-//     var docViewBottom = docViewTop + $(window).height();
-
-//     var elemTop = $(elem).offset().top;
-//     var elemBottom = elemTop + $(elem).height();
-
-//     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-// }
-
-// $(window).scroll(function () {
-//     $('.section').each(function () {
-//         if (isScrolledIntoView(this) === true) {
-//             $(this).addClass('in-view').trigger('classChange');
-//         }
-//     });
-
-//     return false;
-
-// });
 
 $(window).on('scroll', function() {
     var scrolled = $(this).scrollTop();
@@ -218,12 +90,6 @@ $('.section').on('classChange', function(e){
 	TweenMax.staggerTo('#features_automation.in-view .col-md-4', 1, {opacity:1}, 0.2);
 	TweenMax.staggerTo('#features_tools.in-view .resp-tab-content .text-right',1, {opacity:1}, 0,2);
 	TweenMax.staggerFromTo('#free-account.in-view .lang', 1, {opacity:0}, {opacity: 1}, 0.2);
-
-
-
-
-
-
 	TweenMax.staggerFromTo('.in-view #different_languages li', 2, {
 		scale:0.2,
 		ease:Elastic.easeIn,
@@ -234,14 +100,6 @@ $('.section').on('classChange', function(e){
 
 	$(this).off(e);
 })
-
-// var svg = new Walkway({
-//   selector: '#Layer_1',
-//   duration: '5000'
-// }).draw();
-
-
-
 
 var b = $('body'),
 		aes = b.find('.animate-earth-small'),
@@ -262,7 +120,6 @@ var sliderScene = b.find('svg.drawsvg').drawsvg({
 		TweenMax.to(ael, 2.5, {opacity: 1});
 		TweenMax.to(aes, 90, {ease: Power0.easeNone, rotation: "+=360", transformOrigin: 'center center', repeat:-1, force3D: true })
 		TweenMax.to(ael, 95, {ease: Power0.easeNone, rotation: "-=360", transformOrigin: 'center center', repeat:-1, force3D: true })
-		// $(b).find('#free-account').css({"background-image": "url('../img/globe.png')"})
 	}
 });
 
@@ -292,7 +149,4 @@ TweenMax.staggerFrom(".lang .third_batch", 0.5,{
 	force3D:true},
 	.5)
 
-
-
-// TweenMax.staggerFrom( '#react, #css3, #html5, #js, #pug, #git, #github, #netlify', 2, {scale:0.5, opacity:0, delay:1, ease:Elastic.easeOut, force3D:true}, 0.2);
 
